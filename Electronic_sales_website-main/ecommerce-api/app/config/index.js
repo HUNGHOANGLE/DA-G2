119b31b13ee5b4160d618024d06bd1f48231cd67
LeHuuHoangHung <hunghuu3010@gmail.com>
2023-04-06 20:22:00 +0700

    fist commit

---
+++ Electronic_sales_website-main/ecommerce-api/app/config/index.js
@@ -0,0 +1,21 @@
+const config = {
+  database: {
+    connection: 'postgres://xwgeygnw:9sVtHWvLF5T88utbDk4VDuUAeOubjZNT@trumpet.db.elephantsql.com/xwgeygnw',
+  },
+  api: {
+    host: "0.0.0.0",
+    port: 3001,
+  },
+  jwt: {
+    secretKey: "secretKey123",
+  },
+  sendgrid: {
+    secretKey: null,
+    fromEmail: null,
+  },
+  webClient: {
+    url: "https://ecommerce-api-ask7.onrender.com",
+  },
+};
+
+module.exports = config;
